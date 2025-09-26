6b2fd225b1cfa1e8a43a5e63a6829ebb64f9f986
e5can0r <engineer9465@gmail.com>
2025-09-26 23:28:30 +0530

    update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ import messageRoutes from './routes/messageRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5000', 'https://your-frontend.vercel.app'],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
@@ -64,7 +67,7 @@ io.on('connection', (socket) => {
 
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => server.listen(process.env.PORT, () => {
+  .then(() => server.listen(process.env.PORT || 5000, () => {
     console.log(`Server running on ${process.env.PORT}`);
   }))
   .catch((err) => console.error(err));
